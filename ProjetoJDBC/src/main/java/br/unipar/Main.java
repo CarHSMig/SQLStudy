8a792b5b785a32ec2565c81ca67d7851e1886f09
Carlos Henrique dos Santos Migon <carloshenriquedossantosmigon@gmail.com>
2024-05-21 22:39:11 -0300

    Add files via upload

--- ProjetoJDBC/src/main/java/br/unipar/Main.java
+++ ProjetoJDBC/src/main/java/br/unipar/Main.java
@@ -10,7 +10,12 @@ public class Main {
 
     public static void main(String[] args) {
         crateTableUser();
+
+        //inserirUsuario("Taffe", "12345", "Fabio", "1890-01-01");
+
+        listarTodosUsuarios();
     }
+
 
     public static Connection connection() throws SQLException {
         //localhost -> Onde está o banco
@@ -37,4 +42,55 @@ public class Main {
             exception.printStackTrace();
         }
     }
+
+    public static void inserirUsuario(String username, String password, String nome, String dataNascimento) {
+        try {
+
+            //Abre a conexão com o banco
+            Connection conn = connection(); // em caso de problema o catch é executado // in case of problem, catch is executed
+
+            //Prepara a execução de um SQL
+            PreparedStatement preparedStatement = conn.prepareStatement(
+                    "INSERT INTO usuarios (username, password, nome, nascimento)"
+
+                            + "VALUES(?,?,?,?)"
+
+            );
+
+            preparedStatement.setString(1, username);
+            preparedStatement.setString(2, password);
+            preparedStatement.setString(3, nome);
+            preparedStatement.setDate(4, java.sql.Date.valueOf(dataNascimento));
+
+            preparedStatement.executeUpdate();
+
+            System.out.printf("usuario inserido com sucesso");
+
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    public static void listarTodosUsuarios(){
+
+        try {
+            Connection conn = connection();
+
+            Statement statement = conn.createStatement();
+
+            ResultSet result = statement.executeQuery("SELECT * FROM usuarios");
+
+            while (result.next()) {
+                System.out.println(result.getInt("codigo"));
+
+            }
+        }
+
+        catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+
+    }
+
 }
