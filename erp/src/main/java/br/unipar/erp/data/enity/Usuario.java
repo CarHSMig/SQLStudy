4dc9bdbc93a91622ad808017a2792cc9ba9138bb
Carlos Henrique dos Santos Migon <carloshenriquedossantosmigon@gmail.com>
2024-05-28 22:41:16 -0300

    Add files via upload

--- erp/src/main/java/br/unipar/erp/data/enity/Usuario.java
+++ erp/src/main/java/br/unipar/erp/data/enity/Usuario.java
@@ -1,16 +1,36 @@
 package br.unipar.erp.data.enity;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.time.LocalDate;
 
 
 @Entity
+@Getter
+@Setter
+@Table(name="usuarios")
+@AllArgsConstructor
+@NoArgsConstructor
 public class Usuario {
 
     @Id
     private Integer codigo;
+
+    @Column(name="username")
     private String usuario;
+
+    @Column(name = "password")
     private String senha;
+
+    private String nome;
+
+    @Column(name = "nascimento")
     private LocalDate dataNascimento;
 }
